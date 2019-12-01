b926463c922c69e61b2538b9c0836e2c8fb992df
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-01 17:12:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function inc(a) {
 function dec(a) {
   return(a-=1);
 }
-function makeInt() {
+function makeInt(a) {
   return(parseInt('n',10));
-  return(parseInt(0));
+  return(parseInt('0'));
   return(parseInt("nonsense", 10));
 }
 
