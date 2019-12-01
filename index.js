bd6a63860656e1d35c3981dc075c424643694817
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-01 17:28:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function makeInt(a) {
 function preserveDecimal(a) {
   return(parseFloat('2.222'));
   var a = parseFloat('sldkjflksjf');
-  if(nsNAN(a)) {
+  if(nsNAN('sldkjflksjf')) {
     return(true);
   }
 }
