4c96f6ce8af633b73bf29df780760d934ae367eb
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-01 16:41:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+  return(a+b);
+}
+function subtract(a,b) {
+  return(a-b);
+}
+function multiply(a,b) {
+  return(a*b);
+}
+function divide(a,b) {
+  return(a/b);
+}
+function inc(a) {
+  return(a+=1);
+}
+function dec(a) {
+  return(a-=1);
+}
+function makeInt(a) {
+  var a = parseInt('a',10);
+  var b = parseInt('0x2328', 0);
+  var c = parseInt("nonsense", 10);
+}
+
+function preserveDecimal(a) {
+ return(parseFloat(2.222));
+ return(parseFloat("sldkjflksjf"));
+ }
